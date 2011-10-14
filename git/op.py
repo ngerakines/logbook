b8a8a8aeb7a7a0b988c1fe42f3dd52234705dbd5
Norberto Lopes <nlopes.ml@gmail.com>
2011-10-14 18:44:59 +0100

    make sure big output from git log is not a problem (I know python docs say to be careful with communicate, but if you look at the source, it is good enough for practically all projects)

--- git/op.py
+++ git/op.py
@@ -1,5 +1,5 @@
 import os
-import urllib
+import time
 import tempfile
 import subprocess
 
@@ -17,15 +17,15 @@ Check the PRETTY FORMATS section in git log help.
 """
     commits = None
     commits_proc = subprocess.Popen(
-        ['git', 'log', '--pretty=format:"%h ::: %an ::: %aD ::: %s ::: %d"'],
+        ['git', '--no-pager', 'log', '--pretty=format:"%h ::: %an ::: %aD ::: %s ::: %d"'],
         stdout=subprocess.PIPE, close_fds=True, cwd=tmpd)
 
-    # XXX: SOME ERROR CHECKING MISSING HERE.
-    if commits_proc.wait() == 0:
-        # get the parts of the commit into a list
-        # XXX: optimize this shit, it's a mess
-        commits = [[s.strip("\"").strip() for s in rev.strip().split(":::")] for rev in commits_proc.stdout]
-        commits = [(commit[0].strip(), commit[1].strip(),
-                    commit[2].strip(), commit[3].strip(),
-                    commit[4].strip()) for commit in commits]
+    stdout = commits_proc.communicate()[0]
+
+    # get the parts of the commit into a list
+    # XXX: optimize this shit, it's a mess
+    commits = [[s.strip("\"").strip() for s in rev.strip().split(":::")] for rev in stdout.split("\n")]
+    commits = [(commit[0].strip(), commit[1].strip(),
+                commit[2].strip(), commit[3].strip(),
+                commit[4].strip()) for commit in commits]
     return commits
